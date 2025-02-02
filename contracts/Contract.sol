24a89b4cf6097f00c8c7046f5df24980741b5c6d
wesley <wesleywaka77@gmail.com>
2025-02-02 13:16:39 +0300

    feat:types and addFeedBack coontract

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MyContract {
-    constructor() {}
+contract FeedbackStorage {
+    struct Sentiment {
+        int256 score;
+        uint256 numWords;
+        uint256 numHits;
+        uint256 average;
+        string sentimentType;
+        string locale;
+        string vote;
+    }
+
+    struct Feedback {
+        string text;
+        Sentiment[] sentiments;
+        address user;
+    }
+
+    Feedback[] public feedbacks;
+
+    function addFeedback(
+        string memory _text,
+        int256[] memory _scores,
+        uint256[] memory _numWords,
+        uint256[] memory _numHits,
+        uint256[] memory _averages,
+        string[] memory _types,
+        string[] memory _locales,
+        string[] memory _votes
+    ) public {
+        require(
+            _scores.length == _numWords.length &&
+                _numWords.length == _numHits.length &&
+                _numHits.length == _averages.length &&
+                _averages.length == _types.length &&
+                _types.length == _locales.length &&
+                _locales.length == _votes.length,
+            "Input arrays must have the same length"
+        );
+
+        // Create a new Feedback struct
+        Feedback storage newFeedback = feedbacks.push();
+        newFeedback.text = _text;
+        newFeedback.user = msg.sender;
+
+        // Add sentiments to the new feedback
+        for (uint256 i = 0; i < _scores.length; i++) {
+            newFeedback.sentiments.push(
+                Sentiment({
+                    score: _scores[i],
+                    numWords: _numWords[i],
+                    numHits: _numHits[i],
+                    average: _averages[i],
+                    sentimentType: _types[i],
+                    locale: _locales[i],
+                    vote: _votes[i]
+                })
+            );
+        }
+    }
 }
