5061042bbffea8eb414e6ecb6045bdbdc7df4f1b
wesley <wesleywaka77@gmail.com>
2025-02-02 13:22:10 +0300

    chore:add descriptive comments

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,20 @@
 const { ethers } = require('hardhat');
 
 async function main() {
-  // Get the ContractFactory for the FeedbackStorage contract
+  // Used to Get the ContractFactory for the FeedbackStorage contract
   const FeedbackStorage = await ethers.getContractFactory('FeedbackStorage');
 
-  // Deploy the contract
+  // Deploy the Feedback contract
   const feedbackStorage = await FeedbackStorage.deploy();
 
   // Wait for the contract to be deployed
   await feedbackStorage.deployed();
 
-  // Log the contract address
+  // Log the contract address onCompletion of ddeployment
   console.log('FeedbackStorage deployed to:', feedbackStorage.address);
 }
 
-// Run the deployment script
+// Run the deployment script and console error else exit onCompletion
 main()
   .then(() => process.exit(0))
   .catch((error) => {
